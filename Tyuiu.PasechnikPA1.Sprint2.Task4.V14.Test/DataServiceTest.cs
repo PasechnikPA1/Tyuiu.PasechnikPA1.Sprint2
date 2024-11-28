1de7b7acb7fabb5298801b12bcb16f3ae2cb28e2
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-29 00:32:40 +0500

    task4

---
+++ Tyuiu.PasechnikPA1.Sprint2.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PasechnikPA1.Sprint2.Task4.V14.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint2.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 81;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
