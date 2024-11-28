674ac7363f76ef605242ff689c9c551d518f7e18
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-29 00:04:27 +0500

    Add project files.

---
+++ Tyuiu.PasechnikPA1.Sprint2.Task0.V14/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.PasechnikPA1.Sprint2.Task0.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
