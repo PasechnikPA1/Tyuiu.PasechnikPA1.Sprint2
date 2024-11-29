771638a5582ba096830c309356edfcd41cdb5d95
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-29 08:21:43 +0500

    task7

---
+++ Tyuiu.PasechnikPA1.Sprint2.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PasechnikPA1.Sprint2.Task7.V8.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint2.Task7.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
